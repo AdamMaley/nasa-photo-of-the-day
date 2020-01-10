555cfc59be2d09d50f5f0cc6bc0a8d6f50225804
AdamMaley <adamemaley@yahoo.com>
2020-01-09 19:53:01 -0500

    got api rendering

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Card.scss";
 
-function Card() {
+function Card(props) {
+    console.log(props);
     return (
         <div>
-            <h1>{props.title}</h1>
-            <img>{props.image}</img>
-            <p>{props.text}</p>
+            <h1>{props.data.title}</h1>
+            <p>{props.data.date}</p>
+            <img src={props.data.url} alt="Perihelion to Aphelion"/>
+            <p>{props.data.explanation}</p>
         </div>
     )
 }
